a0d33ca9af72e7fa2bc7e25283ad29a971df4424
Hamidreza Maddah <maddahhami@brava.ecs.vuw.ac.nz>
2013-09-03 15:56:24 +1200

    Working on fostering first page database

--- db/migrate/20130826023518_create_fosterers.rb
+++ db/migrate/20130826023518_create_fosterers.rb
@@ -1,23 +1,7 @@
 class CreateFosterers < ActiveRecord::Migration
   def change
     create_table :fosterers do |t|
-      t.string :firstname
-      t.string :lastname
-      t.string :unitNumber
-      t.string :streetNumber
-      t.string :street
-      t.string :suburb
-      t.string :city
-      t.string :zipCode
-      t.string :email
-      t.string :homecontact
-      t.string :mobilecontact
-      t.string :workcontact
-      t.string :theirname
-      t.string :homecontact2
-      t.string :mobilecontact2
-      t.string :workcontact2
-      t.string :relationship
+
       t.string :kitcat
       t.string :p1
       t.string :p2
@@ -30,13 +14,7 @@ class CreateFosterers < ActiveRecord::Migration
       t.string :p4
       t.integer :p51
       t.integer :p52
-      t.integer :p53
-      t.string :personal1
-      t.string :personal1if
-      t.string :personal2
-      t.string :personal3
-      t.string :personal4
-      t.string :personal5      
+      t.integer :p53   
       
       
       t.timestamps
