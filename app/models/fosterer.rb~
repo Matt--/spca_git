875bc3b50912c636902794a134b8f07f78cf1ec8
Hamidreza Maddah <maddahhami@brava.ecs.vuw.ac.nz>
2013-09-04 17:56:45 +1200

    Fosterer first page is done

--- app/models/fosterer.rb~
+++ app/models/fosterer.rb~
@@ -1,6 +1,8 @@
 class Fosterer < ActiveRecord::Base
   attr_accessible :kitcat, :p1, :p2, :p31,:p32, :p33, :p34, :p35, :p36, :p4, :p51, :p52, :p53,
-		:personal2, :send_email, :catnursing, :catfeeding, :catbottlefeed, :catinjured
+		:personal2, :catnursing, :catfeeding, :catbottlefeed, :catinjured, 
+		:dognursing, :dogfeeding, :dogbottlefeed, :doginjured,
+		:rabgui, :hedgehoginjured, :hedgehogbottle, :birdsinjured, :birdstoung, :livestock
       
   
 end
--- db/migrate/20130826023518_create_fosterers.rb
+++ db/migrate/20130826023518_create_fosterers.rb
@@ -14,8 +14,7 @@ class CreateFosterers < ActiveRecord::Migration
       t.string :p4
       t.integer :p51
       t.integer :p52
-      t.integer :p53   
-      t.string :send_email
+      t.integer :p53
       t.string :personal2
       
       
@@ -23,6 +22,18 @@ class CreateFosterers < ActiveRecord::Migration
       t.boolean :catfeeding
       t.boolean :catbottlefeed
       t.boolean :catinjured
+      
+      t.boolean :dognursing
+      t.boolean :dogfeeding
+      t.boolean :dogbottlefeed
+      t.boolean :doginjured
+      
+      t.boolean :rabgui
+      t.boolean :hedgehoginjured
+      t.boolean :hedgehogbottle
+      t.boolean :birdsinjured
+      t.boolean :birdstoung
+      t.boolean :livestock
       
       
       t.timestamps
