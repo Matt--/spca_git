2d007c27273ec3b428db6a30a06fdc735d6fa4dd
Matt Stevens <matt@stevens.net.nz>
2013-09-04 20:01:23 +1200

    fixed tests, added another

---
+++ spec/requests/confirmationEmail_spec.rb
@@ -0,0 +1,39 @@
+ require 'spec_helper'
+
+
+describe "Home" do
+  subject{ page }
+  describe "Index page" do
+    before { visit root_path }
+
+    it { should have_title('SPCA') }
+    it { should have_content('SPCA Volunteer Portal') }
+
+  end
+end
+
+
+describe 'Volunteer' do
+  
+  describe 'saves thier application form' do
+
+
+    describe 'sends a confirmation email' do
+      before {visit volunteers_path }
+    
+      volunteer = Volunteer.new
+      message = volunteer.test_email_confirmation_message
+  
+      test = 
+          "    From: test from\n"+
+          "    To: test to\n"+
+          "    Subject: test header\n\n"+
+          "    test content\n    \n"
+
+      it 'should pass' do
+        assert message[0..-1] == test[0..-1],
+         "failed- message is...\n" + message + test.length.to_s()
+      end
+    end
+  end
+end
