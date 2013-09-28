b1233a3e97467a9e1a0a54613ed8f1b65197c4ee
Matt Stevens <matt@stevens.net.nz>
2013-09-28 18:43:20 +1200

    all works after merge

--- db/migrate/20130906015727_create_dojobs2.rb
+++
@@ -1,9 +0,0 @@
-class CreateDojobs2 < ActiveRecord::Migration
-  def change
-    create_table :dojobs do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130927051613_create_people.rb
+++ db/migrate/20130927051613_create_people.rb
@@ -1,11 +1,11 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-      t.string, :name
-      t.date, :dob
+      t.string :name
+      t.date :dob
       t.string :email
-      t.string, :homePhone
-      t.string, :workPhone
+      t.string :homePhone
+      t.string :workPhone
       t.string :mobile
 
       t.timestamps
