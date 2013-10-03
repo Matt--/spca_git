d29c66fde3a56c6a04b21febf0dee031482de6ec
Hamidreza Maddah <maddahhami@kipp-cafe.ecs.vuw.ac.nz>
2013-10-03 18:21:58 +1300

    Two fosterer forms are merged and it works fine

--- app/models/fosterer.rb
+++ app/models/fosterer.rb
@@ -1,8 +1,17 @@
 class Fosterer < ActiveRecord::Base
   attr_accessible :motivation, :catnursing, :catfeeding, :catbottlefeed, :catinjured, 
-		:dognursing, :dogfeeding, :dogbottlefeed, :doginjured,
-		:rabgui, :hedgehoginjured, :hedgehogbottle, :birdsinjured, :birdstoung, :livestock,
-		:volunteer_id
+		  :dognursing, :dogfeeding, :dogbottlefeed, :doginjured,
+		  :rabgui, :hedgehoginjured, :hedgehogbottle, :birdsinjured, :birdstoung, :livestock,
+		  :volunteer_id,
+		  :ownhome, :inspecting, :rooms, :roomwarm, :hutch, 
+		  :rabbithutch, :aviary, :livestockfenced,
+		  :homedesc, :numadulta, :numchild, :ages,
+		  :numhoursperday, :worktype, :secondperson, 
+		  :allmembersagree, :bringcentre, :transportdesc,
+		  :agreement
+  
+  validates :agreement, :presence => true
+  
       
   belongs_to :volunteer
 end
